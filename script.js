c53806b154d38d866176ee5e0166b307db7cd264
Krushulang <krushulang@gmail.com>
2023-10-14 14:21:47 -0700

    PEE

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+var league = document.getElementById("league");
+var team1 = document.getElementById("team1")
+var team2 = document.getElementById("team2")
+var leagueTeams = ["default"];
+
+
+function teamUpdate(){
+    switch(league.value) {
+        case 'premierleague' : 
+            leagueTeams = ["dreamte", "lamete"];
+            break;
+        case 'laliga' : 
+            leagueTeams = ["drea", "lamet"];
+            break;
+        case 'seriea' : 
+            leagueTeams = ["drmteam", "meteam"];
+            break;
+        case 'league1' : 
+            leagueTeams = ["dreaeam", "lateam"];
+            break;
+        case 'bundesliga' : 
+            leagueTeams = ["dmteam", "lameam"];
+            break;
+        case 'mls' : 
+            leagueTeams = ["dreamteam", "lameteam"];
+            break;
+    };
+    teamOptions = "";
+    for(let i = 0; i < leagueTeams.length; i++){
+        teamOptions += "<option value=" + leagueTeams[i] + ">" + leagueTeams[i] + "</option>"
+    }
+    team1.innerHTML = teamOptions;
+    team2.innerHTML = teamOptions;
+};
 
